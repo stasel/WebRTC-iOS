dbff7b3d547502c5572739afa9172c749f19962b
Stasel <2033301+stasel@users.noreply.github.com>
2020-07-02 13:24:18 +0200

    Added WebRTC swift package

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "WebRTC",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(
+            name: "WebRTC",
+            targets: ["WebRTCFramework"]),
+    ],
+    dependencies: [ ],
+    targets: [
+        .binaryTarget(
+            name: "WebRTCFramework",
+            url: "https://www.dropbox.com/s/pmllo9l0pw2dcax/WebRTC.xcframework.zip?dl=1",
+            checksum: "f31285485f4b4aa26fd0861b957a5356a7ded4279bcc5fce8fbc33d10e18028f"
+        ),
+    ]
+)
