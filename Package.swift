0ed9b2a400a8e38ce6e8fbb34071add7057007f9
Stasel <2033301+stasel@users.noreply.github.com>
2020-07-02 15:16:30 +0200

    Update remote url

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTCFramework",
-            url: "https://www.dropbox.com/s/qb4d0mwr1mvdl56/WebRTC.xcframework.zip?dl=1",
+            url: "https://www.dropbox.com/s/dl/qb4d0mwr1mvdl56/WebRTC.xcframework.zip",
             checksum: "f31285485f4b4aa26fd0861b957a5356a7ded4279bcc5fce8fbc33d10e18028f"
         ),
     ]
